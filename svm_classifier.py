a21c1313af456a1791a24da55ce3832b51bc37d1
Sinchan <sinchan.tex@gmail.com>
2022-06-24 21:52:02 +0530

    ploy_svc

--- svm_classifier.py
+++ svm_classifier.py
@@ -1,43 +1,42 @@
 import streamlit as st
-from sklearn import datasets
 import numpy as np
 
-from sklearn.model_selection import train_test_split
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.svm import SVC
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.datasets import make_moons, make_circles, make_classification
 from matplotlib import pyplot as plt
 from matplotlib.colors import ListedColormap
+from sklearn.model_selection import train_test_split
+from sklearn.svm import LinearSVC
+from sklearn.datasets import make_moons
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.preprocessing import StandardScaler
 
 # frontend ui section
-st.title('Visualizing different sklearn generated dataset')
+st.title('Polyomial SVM classifier in action')
 
 st.write("""
-#### Checking the influence of various parameters/arguments on the sample data points on the built-in make_datasets from scikit-learn.
-make_moons, make_circles & make_classification are scikit-learns's generated dataset which creates two interleaving data points for classification purposes.
-Used to visualize clustering and classification algorithms.
-""")
+#### Checking real-time implementation of SVM classifier on the sklearn generated dataset: `make_moons`.""")
 np.meshgrid()
 d_name = st.sidebar.selectbox("Select sklearn dataset", ("moons", "circles", "classification"))
+
+x0s = np.linspace(-1.5, 2.5, 100)
 
 # dataset parameters function
 def add_params(d_name):
     params = dict()
     if d_name == "moons":
-        n_samples = st.sidebar.slider("no. of samples", 1, 100)
+        n_samples = st.sidebar.slider("no. of samples", 2, 100)
         noise = st.sidebar.slider("noise", 0.01, 1.00)
         params["n_samples"] = n_samples
         params["noise"] = noise
     elif d_name == "circles":
-        n_samples = st.sidebar.slider("no. of samples", 1, 100)
+        n_samples = st.sidebar.slider("no. of samples", 2, 100)
         noise = st.sidebar.slider("noise", 0.01, 1.00)
         factor = st.sidebar.slider("factor", 0.01, 1.00)
         params["n_samples"] = n_samples
         params["noise"] = noise
         params["factor"] = factor
     else:
-        n_samples = st.sidebar.slider("no. of samples", 1, 100)
+        n_samples = st.sidebar.slider("no. of samples", 2, 100)
         n_features = st.sidebar.slider("no. of features", 2, 100)
         n_informative = st.sidebar.slider("no. of informative features", 2, 100)
         params["n_samples"] = n_samples
@@ -59,13 +58,31 @@ def set_dataset(d_name, params):
 
 X, y = set_dataset(d_name, params)
 
+polynomial_svm_clf = Pipeline([
+        ("poly_features", PolynomialFeatures(degree=3)),
+        ("scaler", StandardScaler()),
+        ("svm_clf", LinearSVC(C=10, loss="hinge", random_state=42))
+    ])
+
+polynomial_svm_clf.fit(X, y)
+
 # plotting section
 fig = plt.figure()
-plt.plot(X[:, 0][y==0], X[:, 1][y==0], "bs")
-plt.plot(X[:, 0][y==1], X[:, 1][y==1], "g^")
-#plt.axis([-1.5, 2.5, -1, 1.5])
+plt.plot(X[:, 0][y==0], X[:, 1][y==0], "ko")
+plt.plot(X[:, 0][y==1], X[:, 1][y==1], "rs")
+plt.axis([-1.5, 2.5, -1, 1.5])
 plt.grid(True, which='both')
 plt.xlabel(r"$x_1$", fontsize=20)
 plt.ylabel(r"$x_2$", fontsize=20, rotation=0)
+
+# initialization section
+x0s = np.linspace(-1.5, 2.5, 100)
+x1s = np.linspace(-1.0, 1.5, 100)
+x0, x1 = np.meshgrid(x0s, x1s)
+X = np.c_[x0.ravel(), x1.ravel()]
+y_pred = polynomial_svm_clf.predict(X).reshape(x0.shape)
+y_deci = polynomial_svm_clf.decision_function(X).reshape(x0.shape)
+plt.contourf(x0, x1, y_pred, cmap=plt.cm.brg, alpha=0.2)
+plt.contourf(x0, x1, y_deci, cmap=plt.cm.brg, alpha=0.1)
 
 st.pyplot(fig)
