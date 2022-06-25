0606d68eb87d7f3fec9b6dc72960e49100236a7c
Sinchan <sinchan.tex@gmail.com>
2022-06-25 19:33:25 +0530

    added seaborn theme

--- svm_classifier.py
+++ svm_classifier.py
@@ -1,6 +1,7 @@
 # important libraries
 import streamlit as st
 import numpy as np
+import seaborn as sns
 from matplotlib import pyplot as plt
 from matplotlib.colors import ListedColormap
 from sklearn.model_selection import train_test_split
@@ -10,6 +11,8 @@ from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.preprocessing import StandardScaler
 
+sns.set_theme()
+
 # info section
 st.title('Linear SVM classifier in action')
 
@@ -17,6 +20,7 @@ st.write("""
 #### Checking real-time implementation of Linear SVM classifier using polynomial features on the sklearn generated dataset: `make_moons`.""")
 
 # sample size & noise sliders
+st.sidebar.title("Data points distribution:")
 n_samples = st.sidebar.slider("No. of samples", 2, 100)
 noise = st.sidebar.slider("Noise", 0.01, 1.00)
 
@@ -42,9 +46,11 @@ y_decision = polynomial_svm_clf.decision_function(X_concat).reshape(x0.shape)
 # matplotlib colormap selection dropdown
 color_maps_list = ('Accent', 'Accent_r', 'Blues', 'Blues_r', 'BrBG', 'BrBG_r', 'BuGn', 'BuGn_r', 'BuPu', 'BuPu_r', 'CMRmap', 'CMRmap_r', 'Dark2', 'Dark2_r', 'GnBu', 'GnBu_r', 'Greens', 'Greens_r', 'Greys', 'Greys_r', 'OrRd', 'OrRd_r', 'Oranges', 'Oranges_r', 'PRGn', 'PRGn_r', 'Paired', 'Paired_r', 'Pastel1', 'Pastel1_r', 'Pastel2', 'Pastel2_r', 'PiYG', 'PiYG_r', 'PuBu', 'PuBuGn', 'PuBuGn_r', 'PuBu_r', 'PuOr', 'PuOr_r', 'PuRd', 'PuRd_r', 'Purples', 'Purples_r', 'RdBu', 'RdBu_r', 'RdGy', 'RdGy_r', 'RdPu', 'RdPu_r', 'RdYlBu', 'RdYlBu_r', 'RdYlGn', 'RdYlGn_r', 'Reds', 'Reds_r', 'Set1', 'Set1_r', 'Set2', 'Set2_r', 'Set3', 'Set3_r', 'Spectral', 'Spectral_r', 'Wistia', 'Wistia_r', 'YlGn', 'YlGnBu', 'YlGnBu_r', 'YlGn_r', 'YlOrBr', 'YlOrBr_r', 'YlOrRd', 'YlOrRd_r', 'afmhot', 'afmhot_r', 'autumn', 'autumn_r', 'binary', 'binary_r', 'bone', 'bone_r', 'brg', 'brg_r', 'bwr', 'bwr_r', 'cividis', 'cividis_r', 'cool', 'cool_r', 'coolwarm', 'coolwarm_r', 'copper', 'copper_r', 'cubehelix', 'cubehelix_r', 'flag', 'flag_r', 'gist_earth', 'gist_earth_r', 'gist_gray', 'gist_gray_r', 'gist_heat', 'gist_heat_r', 'gist_ncar', 'gist_ncar_r', 'gist_rainbow', 'gist_rainbow_r', 'gist_stern', 'gist_stern_r', 'gist_yarg', 'gist_yarg_r', 'gnuplot', 'gnuplot2', 'gnuplot2_r', 'gnuplot_r', 'gray', 'gray_r', 'hot', 'hot_r', 'hsv', 'hsv_r', 'inferno', 'inferno_r', 'jet', 'jet_r', 'magma', 'magma_r', 'nipy_spectral', 'nipy_spectral_r', 'ocean', 'ocean_r', 'pink', 'pink_r', 'plasma', 'plasma_r', 'prism', 'prism_r', 'rainbow', 'rainbow_r', 'seismic', 'seismic_r', 'spring', 'spring_r', 'summer', 'summer_r', 'tab10', 'tab10_r', 'tab20', 'tab20_r', 'tab20b', 'tab20b_r', 'tab20c', 'tab20c_r', 'terrain', 'terrain_r', 'turbo', 'turbo_r', 'twilight', 'twilight_r', 'twilight_shifted', 'twilight_shifted_r', 'viridis', 'viridis_r', 'winter', 'winter_r')
 
-color_map = st.sidebar.selectbox("Select contour color-map", color_maps_list)
+st.sidebar.title("Select contour color-map")
+color_map = st.sidebar.selectbox("", color_maps_list)
 
 # contour alpha value sliders
+st.sidebar.title("Contour alpha sliders:")
 alpha_prediction = st.sidebar.slider("Prediction alpha", 0.0, 1.0)
 alpha_decision = st.sidebar.slider("Decision Boundary alpha", 0.0, 1.0)
 
