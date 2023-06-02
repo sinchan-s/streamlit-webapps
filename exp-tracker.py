1ddcbf162f08c9d38092df31b9d9ebdc4fd548c4
Sinchan <sinchan.tex@gmail.com>
2023-06-02 23:57:33 +0530

    nosql-db app init

---
+++ exp-tracker.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import plotly.graph_objects as go
+
+incomes = ["Salary", "Dividend", "Invest Return"]
+expenses = ['Food', 'Loan', 'CC Bill', 'Others']
+currency = "INR"
+page_title = ":money_with_wings:"
