b3f4c8d9e6b6c0260658e1573af9632aa5b4e263
Sinchan <sinchan.tex@gmail.com>
2023-06-17 23:50:13 +0530

    snakey chart function

--- expense_tracker_app/main.py
+++ expense_tracker_app/main.py
@@ -136,7 +136,73 @@ if selected == 'My Wallet Viewer':
     # combi_df = df.join(walletId_df, on=['wallet_id', 'id'], how='left')
     with st.expander('Wallet ID data'):
         st.dataframe(walletId_df)
+
+    #?----SankeyChart wrapper function from https://medium.com/kenlok/how-to-create-sankey-diagrams-from-dataframes-in-python-e221c1b4d6b0
+    def genSankey(df,cat_cols=[],value_cols='',title='Sankey Diagram'):
+        # maximum of 6 value cols -> 6 colors
+        colorPalette = ['#4B8BBE','#306998','#FFE873','#FFD43B','#646464']
+        labelList = []
+        colorNumList = []
+        for catCol in cat_cols:
+            labelListTemp =  list(set(df[catCol].values))
+            colorNumList.append(len(labelListTemp))
+            labelList = labelList + labelListTemp
+            
+        # remove duplicates from labelList
+        labelList = list(dict.fromkeys(labelList))
+        
+        # define colors based on number of levels
+        colorList = []
+        for idx, colorNum in enumerate(colorNumList):
+            colorList = colorList + [colorPalette[idx]]*colorNum
+            
+        # transform df into a source-target pair
+        for i in range(len(cat_cols)-1):
+            if i==0:
+                sourceTargetDf = df[[cat_cols[i],cat_cols[i+1],value_cols]]
+                sourceTargetDf.columns = ['source','target','count']
+            else:
+                tempDf = df[[cat_cols[i],cat_cols[i+1],value_cols]]
+                tempDf.columns = ['source','target','count']
+                sourceTargetDf = pd.concat([sourceTargetDf,tempDf])
+            sourceTargetDf = sourceTargetDf.groupby(['source','target']).agg({'count':'sum'}).reset_index()
+            
+        # add index for source-target pair
+        sourceTargetDf['sourceID'] = sourceTargetDf['source'].apply(lambda x: labelList.index(x))
+        sourceTargetDf['targetID'] = sourceTargetDf['target'].apply(lambda x: labelList.index(x))
+        
+        # creating the sankey diagram
+        data = dict(
+            type='sankey',
+            node = dict(
+            pad = 15,
+            thickness = 20,
+            line = dict(
+                color = "black",
+                width = 0.5
+            ),
+            label = labelList,
+            color = colorList
+            ),
+            link = dict(
+            source = sourceTargetDf['sourceID'],
+            target = sourceTargetDf['targetID'],
+            value = sourceTargetDf['count']
+            )
+        )
+        
+        layout =  dict(
+            title = title,
+            font = dict(
+            size = 10
+            )
+        )
+        
+        fig = dict(data=[data], layout=layout)
+        return fig
     
+    fig = genSankey(df, cat_cols=['wallet_id','category_id'], value_cols='amount',title='My Wallet Expenses')
+    st.plotly_chart(fig, use_container_width=True)
     # with st.form("saved_periods"):
     #     period = st.selectbox("Select Period:", get_all_periods())
     #     submitted = st.form_submit_button("Plot period")
