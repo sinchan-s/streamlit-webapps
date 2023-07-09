44a19c8ed10cb93a94eacbc7a4f631b8ed0860cf
Sinchan <sinchan.tex@gmail.com>
2023-07-09 15:30:26 +0530

    reformatting+delete option added

--- daily-defects-app.py
+++ daily-defects-app.py
@@ -42,10 +42,11 @@ deta = Deta(DETA_KEY)
 defects_db = deta.Base("defects_db")
 imgs_drive = deta.Drive("defects_imgs")
 
+#*------------------------------------------------------------------------------------------*# DETA functions
 #! defects_db functions
-def upload_data(defect_type, details):
+def upload_data(defect_type, details, remarks):
     date_time = datetime.now()
-    date = date_time.strftime("%m/%d/%Y")
+    date = date_time.strftime("%d-%m-%Y")
     time = date_time.strftime("%H:%M:%S")
     return defects_db.put({"key": key, "date": date, "time": time, 
                     "defect_type": defect_type, "details": details, "remarks": remarks})
@@ -53,8 +54,8 @@ def upload_data(defect_type, details):
 def upload_img(image_name, image_data):
     return imgs_drive.put(image_name, data=image_data)
 
-def fetch_data(period):
-    return defects_db.get(period)
+def fetch_data(key):
+    return defects_db.get(key)
 
 def fetch_all_data():
     res = defects_db.fetch()
@@ -63,26 +64,26 @@ def fetch_all_data():
 def convert_df(df):
     return df.to_csv().encode('utf-8')
 
-
+#*------------------------------------------------------------------------------------------*# NAV-1
 if selected=='Defects Entry':
     #! image display-upload
     placeholder_img = Image.open('place_h.jpg')
-    col1, col2, col3 = st.columns(3) 
+    col1, col2, col3 = st.columns(3)
     cam_img = col1.camera_input("Take defect image")
-    user_imgs = col2.file_uploader("Choose defect image to upload", accept_multiple_files=False, type=['png', 'jpeg', 'jpg'])
+    user_img = col2.file_uploader("Choose defect image to upload", accept_multiple_files=False, type=['png', 'jpeg', 'jpg'])
 
     #! image preview panel
     # Some code from: https://stackoverflow.com/questions/74423171/streamlit-image-file-upload-to-deta-drive
     with col3.expander('image preview'):
-        if cam_img is None and user_imgs is None:
+        if cam_img is None and user_img is None:
             st.image(placeholder_img, caption='Placeholder image')
             image_data = placeholder_img
-        elif user_imgs is None:
+        elif user_img is None:
             st.image(cam_img, caption=key)
             image_data = cam_img.getvalue()
         else:
-            st.image(user_imgs, caption=key)
-            image_data = user_imgs.getvalue()
+            st.image(user_img, caption=key)
+            image_data = user_img.getvalue()
 
     #! details add-on
     defects_list = ['Slubs', 'Splices', 'Warp lining']
@@ -93,7 +94,7 @@ if selected=='Defects Entry':
     po_no = col2.text_input("PO No:")
     k1 = col3.text_input("Article:")
     qty = col3.number_input("Defect quantity observed:")
-    remrk = col3.text_area("Additional Remarks")
+    remarks = col3.text_area("Additional Remarks")
     st.divider()
 
     #! data validate conditions
@@ -106,55 +107,57 @@ if selected=='Defects Entry':
     #! upload button
     col1, col2, col3 = st.columns(3)
     upload_button = col1.button(label='Upload Data')                          #? Upload button
-    image_name = key
     if upload_button:
         details = {'Customer': customer, 'PO': po_no, 'K1': k1, 'Qty': qty}
-        prog_bar = col2.progress(0)
-        upload_data(defect_type, details)                                   #? button for text data upload
-        upload_img(image_name, image_data)                                  #? button for only ref. image upload
+        prog_bar = col2.progress(0) #?progress=0%
+        upload_img(image_name=key, image_data=image_data)                     #? button for image data upload
+        upload_data(defect_type, details, remarks)                            #? button for text data upload
         col3.success("Data Uploaded successfully !!")                         #? upload successful..
-        prog_bar.progress(100)
+        prog_bar.progress(100) #?progress=100%
 
+#*------------------------------------------------------------------------------------------*# NAV-2
 if selected=='Defects History':
     col1, col2 = st.columns(2)
+    
     #! fetch button
-    fetch_button = col1.button(label='Fetch/Refresh')
+    fetch_button = col1.button(label='Fetch / Refresh Data')
     if 'defects_data' not in st.session_state:
-        st.session_state.defects_data = 0
+        st.session_state.defects_data = {'key':0, }
     if fetch_button:
         with col2:
-            prog_bar = st.progress(0)
+            prog_bar = st.progress(0) #?progress=0%
             st.session_state.defects_data = fetch_all_data()
-            prog_bar.progress(100)
+            prog_bar.progress(100) #?progress=100%
+
+    #! dataframing the json data
     df = pd.DataFrame(st.session_state.defects_data)
+    df = df[['key', 'date', 'time', 'defect_type', 'details', 'remarks']].set_index('key')
         
-    omni_key = col1.selectbox("All Defects:", df.key)
-    # defect_select = st.selectbox('Select defect:', df.defect_type)
-    # omni_key = df[df.defect_type.str.contains(defect_select)].key[0]
-    # st.write('omni_key:  '+ omni_key)
+    omni_key = col1.selectbox("Defects key:", df.index)
 
     try:
         col1.image(Image.open(imgs_drive.get(omni_key)))
     except:
         col1.error("No Image available !!")
-    df = df[['key', 'date', 'time', 'defect_type', 'details']].set_index('key')
 
     #! data downloading...
     csv = convert_df(df)
-    st.download_button(label="Download data as CSV", data=csv, file_name='defects_df.csv', mime='text/csv',)
+    col2.download_button(label="Download data as CSV", data=csv, file_name='defects_df.csv', mime='text/csv',)
 
     #! data display
-    with st.expander('Raw Data Preview:'):
-        st.json(st.session_state.defects_data)
-    try:
-        st.table(df)
-    except:
-        st.error("An error occured while dataframing...🙁")
+    st.divider()
+    with st.expander('All Defects Data'):
+        # st.json(st.session_state.defects_data)
+        try:
+            st.dataframe(df)
+        except:
+            st.error("An error occured while dataframing...🙁")
 
     #! delete entry
-    delete_button = st.button(label=f'Delete entry: {omni_key}')
-    # st.write(omni_key)
+    col1, col2 = st.columns(2)
+    delete_key = col1.selectbox("Select entry to delete:", df.index)
+    delete_button = col2.button(label='Delete this entry')
     if delete_button:
-        prog_bar = st.progress(100)
+        prog_bar = st.progress(0) #?progress=0%
         defects_db.delete(omni_key)
-        prog_bar.progress(0)
+        prog_bar.progress(100) #?progress=100%
