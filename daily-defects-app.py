124522ee28a2e83d5745d4e62385058b3340bb0a
Sinchan <sinchan.tex@gmail.com>
2023-07-17 22:03:38 +0530

    reformatting+image update added

--- daily-defects-app.py
+++ daily-defects-app.py
@@ -7,7 +7,7 @@ import streamlit as st
 from streamlit_option_menu import option_menu  #pip install streamlit-option-menu
 # from annotated_text import annotated_text   #pip install st-annotated-text
 
-#! page configuration
+#!------------page configuration
 st.set_page_config(
     page_title="Daily defects observation • web-app",
     page_icon=":octagonal_sign:",
@@ -16,7 +16,7 @@ st.set_page_config(
     menu_items=None
 )
 
-#! clean streamlit styling
+#!------------clean streamlit styling
 hide_default_format = """
        <style>
        #MainMenu {visibility: hidden;}
@@ -26,20 +26,20 @@ hide_default_format = """
        """
 st.markdown(hide_default_format, unsafe_allow_html=True)
 
-#! unique key generator
+#!------------unique key generator
 key = str(datetime.now().timestamp())
 
-#! set title and subtitle
+#!------------set title and subtitle
 st.title('Daily Defects observation app')
 
-#! nav menu
+#!------------nav menu
 selected = option_menu(
     menu_title=None, 
     options=['Defects Entry', 'Defects History'],
     icons=['card-list', 'clipboard2-data'],
     orientation='horizontal')
 
-#! initialize deta Base & Drive
+#!------------initialize deta Base & Drive
 # @st.cache_resource(ttl=3600)
 def load_data():
     DETA_KEY = st.secrets["DETA_KEY"]
@@ -53,7 +53,8 @@ defects_db = conn[0]
 imgs_drive = conn[1]
 
 #*------------------------------------------------------------------------------------------*# DETA functions
-#! defects_db functions
+#*------------------------------------------------------------------------------------------*#
+#!------------defects_db functions
 # @st.cache_data(ttl=3600, show_spinner="uploading...")
 def upload_data(defect_type, customer, article, po_no, qty, remarks):
     return defects_db.put({"key": key, "Date": date, "Defect_type": defect_type, "Customer": customer, "Article": article, "PO": po_no, "Quantity": qty, "Remarks": remarks})
@@ -74,16 +75,17 @@ def convert_df(df):
     return df.to_csv().encode('utf-8')
 
 #*------------------------------------------------------------------------------------------*# NAV-1
+#*------------------------------------------------------------------------------------------*#
 if selected=='Defects Entry':
 
-    #! image display-upload
+    #!------------image display-upload
     col1, col2, col3 = st.columns(3, gap="large")
     placeholder_img = Image.open('place_h.jpg')
     cam_img = col1.camera_input(":camera: Take photo")
     user_img = col2.file_uploader(":frame_with_picture: Upload image", accept_multiple_files=False, type=['png', 'jpeg', 'jpg'])
 
 
-    #! details add-on
+    #!------------details add-on
     defects_list = ['Slubs', 'Splices', 'Lining', 'Patta', 'Dropping', 'SM & TP', 'Leno issue', 'Neps', 'Oil Stain']
     col1, col2, col3 = st.columns(3, gap="large")
     date_time = col1.date_input('Select date 📅:', datetime.now())
@@ -97,18 +99,18 @@ if selected=='Defects Entry':
     remarks = col3.text_area("Additional Remarks:")
     st.divider()
 
-    #! data validate conditions
+    #!------------data validate conditions
     defect_type = ""
     if not defects:
         defect_type = m_defects
     else:
         defect_type = ', '.join(str(d) for d in defects)
     
-    #! upload preview expander
+    #!------------upload preview expander
     # code inspired rom: https://stackoverflow.com/questions/74423171/streamlit-image-file-upload-to-deta-drive
     with st.expander('Preview Upload'):
         col1, col2 = st.columns(2)
-        #! image section
+        #!------------image section
         if cam_img is None and user_img is None:
             col1.image(placeholder_img, caption='Placeholder image', width=350, use_column_width='auto')
             image_data = placeholder_img
@@ -118,7 +120,7 @@ if selected=='Defects Entry':
         else:
             col1.image(user_img, caption=key, width=350)
             image_data = user_img.getvalue()
-        #! details filled-in
+        #!------------details filled-in
         upload_df = {
             'Defect Type': defect_type,
             'Customer': customer,
@@ -131,7 +133,7 @@ if selected=='Defects Entry':
         col2.table(pd.DataFrame(upload_df, index=[key]).T)
 
 
-    #! upload button
+    #!------------upload button
     col1, col2, col3 = st.columns(3, gap="large")
     upload_button = col1.button(label='Upload Data ⬆️', use_container_width=True)                          #? Upload button
     if upload_button:
@@ -143,9 +145,10 @@ if selected=='Defects Entry':
         prog_bar.progress(100) #?progress=100%
 
 #*------------------------------------------------------------------------------------------*# NAV-2
+#*------------------------------------------------------------------------------------------*#
 if selected=='Defects History':
     col1, col2, col3 = st.columns(3, gap="large")
-    #! fetch button
+    #!------------fetch button
     fetch_button = col1.button(label='Fetch / Refresh Data  🔄', use_container_width=True)
     if 'defects_data' not in st.session_state:
         st.session_state.defects_data = {'key':0, }
@@ -155,16 +158,16 @@ if selected=='Defects History':
             st.session_state.defects_data = fetch_all_data()
             prog_bar.progress(100) #?progress=100%
     try:
-        #! dataframing the json data
+        #!------------dataframing the json data
         df = pd.DataFrame(st.session_state.defects_data)
         df = df[["key", "Date", "Defect_type", "Customer", "Article", "PO", "Quantity", "Remarks"]].set_index('key')
         df = df.sort_values(by='key',ascending=False)
 
-        #! data downloading...
+        #!------------data downloading...
         csv = convert_df(df)
         col3.download_button(label="Download Data (.csv)  📥", data=csv, file_name='defects_df.csv', mime='text/csv', use_container_width=True)
         
-        #! all defects dataframe
+        #!------------all defects dataframe
         with st.expander('View All Defects Data'):
             # st.json(st.session_state.defects_data)
             try:
@@ -178,7 +181,7 @@ if selected=='Defects History':
         
         omni_key = st.selectbox("Select Defect(key):", df.index)
 
-        #! defect details preview
+        #!------------defect details preview
         with st.expander(label='Defect details', expanded=True):
             sel_defect = df[df.index==omni_key]
             col1, col2 = st.columns(2, gap="large")
@@ -188,28 +191,42 @@ if selected=='Defects History':
             col1.image(defect_img, caption=f"{sel_defect.Defect_type[0]} in {sel_defect.Quantity[0]}m of {sel_defect.Customer[0]} fabric", width=300, use_column_width='always')
             col2.table(pd.DataFrame(sel_defect).T)
 
-        #! update entry
+        #!------------update entry
         with st.expander('Update data'):
             st.metric('Selected entry:', omni_key)
+            update_pass = st.secrets["UPD_PASS"]
             col1, col2 = st.columns(2, gap="large")
-            update_key = col1.selectbox('Select Field:', df.columns[1:])
-            current_val = col1.caption(f'Current value ({update_key}): {defects_db.get(omni_key)[update_key]}')
-            update_value = col1.text_input('New Value:')
-            if update_key=='Qty':            #? check for numerical value
-                update_value = float(update_value)
-            upd_pass = st.secrets["UPD_PASS"]
+
+            all_fields = list(df.columns[1:])   #?all available fields dropdown
+            all_fields.append('Image')
+            update_key = col1.selectbox('Select Field:', all_fields)
+
+            
+            if update_key=='Quantity':
+                current_val = defects_db.get(omni_key)[update_key]
+                update_value = col1.number_input('New Value:', value=current_val)
+            elif update_key=='Image':
+                update_value = col1.file_uploader(":frame_with_picture: Upload image", accept_multiple_files=False, type=['png', 'jpeg', 'jpg'])
+            else:
+                current_val = defects_db.get(omni_key)[update_key]
+                update_value = col1.text_input('New Value:', value=current_val)
+            # col1.caption(f'Current value ({update_key}): {current_val}')
+                
             input_pass = col2.text_input('Enter Password to update entry:')
             update_status = True
-            if input_pass==upd_pass:    #? getting update access
+            if input_pass==update_pass:    #? getting update access
                 update_status = False
             update_button = col2.button(label='Update this entry', disabled=update_status, use_container_width=True)
             if update_button:
                 prog_bar = st.progress(0) #?progress=0%
-                defects_db.update({update_key: update_value},omni_key)
+                if update_key=='Image':
+                    upload_img(image_name=omni_key, image_data=update_value.getvalue())
+                else:
+                    defects_db.update({update_key: update_value},omni_key)
                 fetch_all_data()
                 prog_bar.progress(100) #?progress=100%
 
-        #! delete entry
+        #!------------delete entry
         with st.expander('Delete data'):
             st.metric('Selected entry:', omni_key)
             col1, col2 = st.columns(2, gap="large")
