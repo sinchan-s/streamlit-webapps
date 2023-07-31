6f13627d8f9f10bf6e7209249e2e136fd430b39b
Sinchan <sinchan.tex@gmail.com>
2023-07-31 23:15:25 +0530

    basic-setup

--- st_dashboards/main.py
+++ st_dashboards/main.py
@@ -1,8 +1,11 @@
 #! Code inspired from Sven Bosau @ https://github.com/Sven-Bo/PyGWalker-Guide-for-Streamlit-and-Jupyter/blob/main/app.py
 import streamlit as st
 import pandas as pd
+import seaborn as sns
 import pygwalker as pyg
 import sqlite3
+from streamlit_option_menu import option_menu  #pip install streamlit-option-menu
+from annotated_text import annotated_text, annotation   #pip install st-annotated-text
 
 #! Set page configuration
 st.set_page_config(
@@ -24,8 +27,16 @@ st.markdown(hide_default_format, unsafe_allow_html=True)
 
 
 #! Set title and subtitle
-st.title('Interactive Data Visualization app')
-# st.subheader('On-the-go Data Visualizing & Analyzing')
+st.title('QA Dashboard')
+st.subheader('All insights in one place:')
+
+#!------------nav menu
+selected = option_menu(
+    menu_title=None, 
+    options=['NCR', 'CUR', 'C-grade(Q3)', 'Sound Surplus(Q6)', 'Complaints'],
+    icons=['asterisk', 'bookmark-x', 'backspace-reverse', 'border-all', 'clipboard-x'],
+    orientation='horizontal')
+
 
 #! upload & read user data
 user_file = st.file_uploader("Upload a CSV file")
@@ -33,7 +44,8 @@ user_file = st.file_uploader("Upload a CSV file")
 if user_file is None:
     # @st.cache_data
     st.warning('No File uploaded: You can still play with demo-data')
-    df = pd.read_csv('tips.csv')
+    # st.write(sns.get_dataset_names())
+    df = sns.load_dataset('diamonds')
 else:
     st.success('File uploaded')
     col1, col2, col3, col4 = st.columns(4)
