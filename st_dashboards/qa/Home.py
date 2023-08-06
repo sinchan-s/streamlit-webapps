36f6520523d46746baac8c2b196e106df8624e0b
Sinchan <sinchan.tex@gmail.com>
2023-08-06 22:03:53 +0530

    nav temps3

--- st_dashboards/qa/Home.py
+++ st_dashboards/qa/Home.py
@@ -40,64 +40,74 @@ selected = option_menu(
 
 
 #*------------------------------------------------------------------------------------------*# 
-#*                                           NAV-1                                          *#
+#*                                         NAV-1: NCR                                       *#
 #*------------------------------------------------------------------------------------------*#
 if selected=='NCR':
 
     #!------------graph display
+    ncr_main_chart = st.bar_chart()
     col1, col2, col3 = st.columns(3, gap="large")
     col1.write('DisplayC')
     col2.write('DisplayT')
     col3.write('DisplayI')
     ncr_chart = col1.bar_chart()
     ncr_chart2 = col2.bar_chart()
+    ncr_df = st.dataframe()
 
 #*------------------------------------------------------------------------------------------*# 
-#*                                           NAV-2                                          *#
+#*                                         NAV-2: CUR                                       *#
 #*------------------------------------------------------------------------------------------*#
 if selected=='CUR':
 
     #!------------graph display
+    cur_main_chart = st.bar_chart()
     col1, col2, col3 = st.columns(3, gap="large")
     col1.write('DisplayC')
     col2.write('DisplayT')
     col3.write('DisplayI')
-    ncr_chart = col1.bar_chart()
-    ncr_chart3 = col3.bar_chart()
+    cur_chart = col1.bar_chart()
+    cur_chart3 = col3.bar_chart()
+    cur_df = st.dataframe()
 
 #*------------------------------------------------------------------------------------------*# 
-#*                                           NAV-3                                          *#
+#*                                         NAV-3: Q3                                        *#
 #*------------------------------------------------------------------------------------------*#
 if selected=='Q3':
 
     #!------------graph display
+    q3_main_chart = st.bar_chart()
     col1, col2, col3 = st.columns(3, gap="large")
     col1.write('DisplayC')
     col2.write('DisplayT')
     col3.write('DisplayI')
-    ncr_chart2 = col2.bar_chart()
-    ncr_chart3 = col3.bar_chart()
+    q3_chart2 = col2.bar_chart()
+    q3_chart3 = col3.bar_chart()
+    q3_df = st.dataframe()
 
 #*------------------------------------------------------------------------------------------*# 
-#*                                           NAV-4                                          *#
+#*                                         NAV-4: Q6                                        *#
 #*------------------------------------------------------------------------------------------*#
 if selected=='Q6':
 
     #!------------graph display
+    q6_main_chart = st.bar_chart()
     col1, col2, col3 = st.columns(3, gap="large")
     col1.write('DisplayC')
     col2.write('DisplayT')
     col3.write('DisplayI')
-    ncr_chart = col1.bar_chart()
+    q6_chart = col1.bar_chart()
+    q6_df = st.dataframe()
 
 #*------------------------------------------------------------------------------------------*# 
-#*                                           NAV-5                                          *#
+#*                                    NAV-5: Complaints                                     *#
 #*------------------------------------------------------------------------------------------*#
 if selected=='Complaints':
 
     #!------------graph display
+    comp_main_chart = st.bar_chart()
     col1, col2, col3 = st.columns(3, gap="large")
     col1.write('DisplayC')
     col2.write('DisplayT')
     col3.write('DisplayI')
-    ncr_chart3 = col3.bar_chart()
+    comp_chart3 = col3.bar_chart()
+    comp_df = st.dataframe()
