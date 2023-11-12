f67d00f1836df89a833bb013fd4c82fe97f2cabb
Sinchan <sinchan.tex@gmail.com>
2023-11-12 22:21:56 +0530

    reformated #1

--- st_dashboards/qa/pages/QA_Reports.py
+++ st_dashboards/qa/pages/QA_Reports.py
@@ -38,10 +38,6 @@ if selected=='Grouping':
     df = pd.read_excel('qa-sept23.xlsx', sheet_name='Data', skiprows=[0], index_col=0)
     st.dataframe(df)
 
-    # all_cols = df.columns
-    # for i,t in enumerate(all_cols):
-    #     st.write(f'Σ {t}',str(df.iloc[-1,i].round(2)),'m')
-    
     col1, col2, col3 = st.columns(3, gap='large')
     with col1:
         st.write('Printed P/D Production: ', df.iloc[-1,0].round(2), 'm')
@@ -52,12 +48,10 @@ if selected=='Grouping':
         st.write('Print FRC: ', df.iloc[-1,7].round(2), 'm')
         st.write('Print RG/RS Production: ', df.iloc[-1,5].round(2), 'm')
         st.write('Total Print Production: ', df.iloc[-1,8].round(2), 'm')
-    
+
     with col2:
         st.write('Bulk YD Production: ', df.iloc[-1,9].round(2), 'm')
         st.write('YD Yardage Production: ', df.iloc[-1,10].round(2), 'm')
-        # st.write('Printed YD Yardage Production', df.iloc[-1,3].round(2), 'm')
-        # st.write('Piece Dyed Production', df.iloc[-1,4].round(2), 'm')
         st.write('YD FRC: ', df.iloc[-1,13].round(2), 'm')
         st.write('YD RG/RS Production: ', df.iloc[-1,11].round(2), 'm')
         st.write('Total YD Production: ', df.iloc[-1,14].round(2), 'm')
