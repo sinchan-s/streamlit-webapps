c2c55b18bcb5c7f89e62520bec43cba6068c3b1a
Sinchan <sinchan.tex@gmail.com>
2023-11-14 21:50:10 +0530

    sum func defined

--- st_dashboards/qa/pages/QA_Reports.py
+++ st_dashboards/qa/pages/QA_Reports.py
@@ -22,6 +22,10 @@ hide_default_format = """
        """
 st.markdown(hide_default_format, unsafe_allow_html=True)
 
+#! calculative functions
+def half_sum(col):
+    return df[col].sum()/2
+
 #! Set title and subtitle
 st.title('QA Reports')
 # st.subheader('Upload file to explore insights:')
@@ -36,6 +40,7 @@ selected = option_menu(
 if selected=='Grouping':
     #!------QA data
     df = pd.read_excel('qa-sept23.xlsx', sheet_name='Data', skiprows=[0], index_col=0)
+    # st.dataframe(df)
 
     st.write(f'Total Production: {(df.iloc[-1,8]+df.iloc[-1,14]).round(2)}m ({df.iloc[-1,8]+df.iloc[-1,14]+df.iloc[-1,4]}m)')
     # st.write('Total Production (including PD Production): ', ().round(2), 'm')
@@ -43,12 +48,12 @@ if selected=='Grouping':
     col1, col2 = st.columns(2, gap='large')
     with col1:
         print_lst_idx = ['Printed P/D Production', 'Printed YD Production', 'Printed P/D Yardage Production', 'Printed YD Yardage Production', 'Piece Dyed Production', 'Print FRC', 'Print RG/RS Production', 'Total Print Production']
-        print_lst_vals = [df.iloc[:,0].sum()/2, df.iloc[:,1].sum()/2, df.iloc[:,2].sum()/2, df.iloc[:,3].sum()/2, df.iloc[:,4].sum()/2, df.iloc[:,7].sum()/2, df.iloc[:,5].sum()/2, df.iloc[:,8].sum()/2]
+        print_lst_vals = [half_sum('Print '), half_sum('Print Yd'), half_sum('Print PD Yardage '), half_sum('Print Yd Yardage '), half_sum('Piece Dyed '), half_sum('Print FRC'), half_sum('Print RG/RS'), half_sum('PRINT TOTAL')]
         print_df = pd.DataFrame(print_lst_vals, index=print_lst_idx, columns=['Qty (m)'])
         st.dataframe(print_df)
 
     with col2:
         yd_lst_idx = ['YD Production', 'YD Yardage Production', 'YD FRC', 'YD RG/RS Production', 'Total YD Production']
-        yd_lst_vals = [df.iloc[:,9].sum()/2, df.iloc[:,10].sum()/2, df.iloc[:,13].sum()/2, df.iloc[:,11].sum()/2, df.iloc[:,14].sum()/2]
+        yd_lst_vals = [half_sum('Yarn Dyed'), half_sum('Yarn Dyed Yardage'), half_sum('Yarn Dyed FRC'), half_sum('YD RG/RS'), half_sum('YD TOTAL')]
         yd_df = pd.DataFrame(yd_lst_vals, index=yd_lst_idx, columns=['Qty (m)'])
         st.dataframe(yd_df)
