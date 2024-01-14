873183c0c6699d676868f97da23d4412aa9442a4
Sinchan <sinchan.tex@gmail.com>
2024-01-14 22:29:13 +0530

    file read check #2

--- quickfibre/main.py
+++ quickfibre/main.py
@@ -2,7 +2,7 @@
 import pandas as pd
 import numpy as np
 import streamlit as st
-import re, pickle
+import re, pickle, os
 from pathlib import Path
 
 #! addtional libs
@@ -26,6 +26,9 @@ hide_default_format = """
        </style>
        """
 st.markdown(hide_default_format, unsafe_allow_html=True)
+
+cwd = os.getcwd()
+st.write(cwd)
 
 #! user authentication
 # names = ['Peter', 'John']
