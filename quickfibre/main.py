8dc24ed7e97c6c537c267521c039882c59598a0d
Sinchan <sinchan.tex@gmail.com>
2024-01-20 23:45:10 +0530

    nav interact

--- quickfibre/main.py
+++ quickfibre/main.py
@@ -1,3 +1,4 @@
+
 #! standard librabries
 import pandas as pd
 import numpy as np
@@ -45,14 +46,19 @@ qf_drive = conn[1]
 #*------------------------------------------------------------------------------------------*#
 #*                                       DETA functions                                     *#
 #*------------------------------------------------------------------------------------------*#
+#? db: user credentials input
 insert_user = lambda username, name, password : qf_db.put({"key": username, "name": name, "password": password})
 
+#? db: get a user data
 user_data = lambda key : qf_db.get(key)
 
+#? db: get all user data
 all_users_data = lambda : qf_db.fetch().items
 
+#? db: update user data
 update_user = lambda updates, username : qf_db.update(updates, username)
 
+#? db: delete user data
 delete_user = lambda username : qf_db.delete(username)
 
 drive_upload = lambda file : qf_drive.put(file.name, data=file)
@@ -68,49 +74,33 @@ left_col.header("QuickFibre")
 
 
 #! products highlight & nav menu
-col1, col2 = st.columns([5, 1])
-with col1:
-    test_items = [
-        dict(
-            title="Vardhman Apparels",
-            text="Our inhouse garmenting facility",
-            interval=None,
-            img="https://www.vardhman.com/images/Businesses/Garments/Banner.jpg",
-        ),
-        dict(
-            title="Vardhman Yarns",
-            text="Prime producer of premium quality yarns",
-            img="https://www.vardhman.com/images/Businesses/Yarns/Banner.jpg",
-        ),
-        dict(
-            title="Vardhman Fabrics",
-            text="Vertically integrated fabric suppliers",
-            img="https://www.vardhman.com/images/Businesses/Fabrics/Banner.jpg",
-        ),
-    ]
-    carousel(items=test_items, width=1)
+test_items = [
+    dict(
+        title="Vardhman Apparels",
+        text="Our inhouse garmenting facility",
+        interval=None,
+        img="https://www.vardhman.com/images/Businesses/Garments/Banner.jpg",
+    ),
+    dict(
+        title="Vardhman Yarns",
+        text="Prime producer of premium quality yarns",
+        img="https://www.vardhman.com/images/Businesses/Yarns/Banner.jpg",
+    ),
+    dict(
+        title="Vardhman Fabrics",
+        text="Vertically integrated fabric suppliers",
+        img="https://www.vardhman.com/images/Businesses/Fabrics/Banner.jpg",
+    ),
+]
 
-with col2:
-    nav_menu = option_menu(None, ["Home", "Variety", "Enquiry", "Account"], 
-        icons=['house', 'cloud-upload', "list-task", 'gear'], 
-        menu_icon="cast", default_index=0, orientation="vertical",
-        styles={
-        "container": {"padding": "0!important", "background-color": "#f1f1f1"},
-        "icon": {"color": "#fccc08", "font-size": "15px"}, 
-        "nav-link": {"color": "black","font-size": "15px", "text-align": "left", "margin":"0px", "--hover-color": "#bfbfbf"},
-        "nav-link-selected": {"background-color": "#008b47"},
-    })
 
-# col1.image('https://www.vardhman.com/images/Businesses/Garments/Banner.jpg')
-# col2.image('https://www.vardhman.com/images/Businesses/Yarns/Banner.jpg')
-# col3.image('https://www.vardhman.com/images/Businesses/Fabrics/Banner.jpg')
 
 #! sidebar contents
-st.sidebar.image('quickfibre/ph.png')
+st.sidebar.image('quickfibre/ph.png')  #? location
 
 #! user account control
 # https://blog.streamlit.io/streamlit-authenticator-part-1-adding-an-authentication-component-to-your-app/
-with open('quickfibre/config.yaml') as file:
+with open('quickfibre/config.yaml') as file:   #? location
     config = yaml.load(file, Loader=SafeLoader)
 hashed_pass = stauth.Hasher(['abc1234', 'def1234']).generate()
 # st.write(hashed_pass)
@@ -125,12 +115,36 @@ name , auth_status, username = authenticator.login('Login', 'sidebar')
 
 if auth_status==False:
     st.sidebar.error('Username/Password is incorrect!!')
+    carousel(items=test_items, width=1)
 elif auth_status==None:
     st.sidebar.warning('Enter credentials to continue')
+    carousel(items=test_items, width=1)
 elif auth_status==True:
-    #! account details
+    col1, col2 = st.columns([4, 1])
+    with col1:
+        st.subheader("Welcome to the world of possibilities !!\nClick on the side controls to navigate")
+    with col2:
+        nav_menu = option_menu(None, ["Home", "Variety", "Enquiry", "Account"], 
+            icons=['house', 'cloud-upload', "list-task", 'gear'], 
+            menu_icon="cast", default_index=0, orientation="vertical",
+            styles={
+            "container": {"padding": "0!important", "background-color": "#f1f1f1"},
+            "icon": {"color": "#fccc08", "font-size": "15px"}, 
+            "nav-link": {"color": "black","font-size": "15px", "text-align": "left", "margin":"0px", "--hover-color": "#bfbfbf"},
+            "nav-link-selected": {"background-color": "#008b47"},
+        })
+    col1, col2, col3 = st.columns(3, gap='large')
+    if nav_menu=="Home":    
+        col1.write('Vardhman Apparels')
+        col1.image('https://www.vardhman.com/images/Businesses/Garments/Banner.jpg')
+        col2.write('Vardhman Yarns')
+        col2.image('https://www.vardhman.com/images/Businesses/Yarns/Banner.jpg')
+        col3.write('Vardhman Fabrics')
+        col3.image('https://www.vardhman.com/images/Businesses/Fabrics/Banner.jpg')
+    #! Sidebar UAC
     with st.sidebar:
-        st.image('quickfibre/user-ph.png')
+        #! user account details
+        st.image('quickfibre/user-ph.png')     #? location
         st.write(f'Welcome, **{name}** !')
         st.caption(f'{username}')
         st.caption('{company}')
@@ -141,12 +155,12 @@ elif auth_status==True:
         col1.button(':mag:', help='Search')
         col2.button(':male-office-worker:', help='Account')
         col3.button(':womans_clothes:', help='Colections')
-        col4.button(':speech_balloon:', help='Chat')
+        col4.button(':speech_balloon:', help='Chat Support')
         authenticator.logout('Logout', 'sidebar')
 
     #! reading the source files
-    articles_df = pd.read_csv("quickfibre/articles.csv",encoding= 'unicode_escape')
-    orders_df = pd.read_csv("quickfibre/sitedata.csv",encoding= 'unicode_escape')
+    articles_df = pd.read_csv("quickfibre/articles.csv",encoding= 'unicode_escape')    #? location
+    orders_df = pd.read_csv("quickfibre/sitedata.csv",encoding= 'unicode_escape')      #? location
 
 
     #! column extraction from construction column
@@ -175,53 +189,54 @@ elif auth_status==True:
     effect_dict = {'Normal': "", 'Seer Sucker': 'SUCKER', 'Crepe': 'CREPE', 'Butta-Cut': 'FIL-COUPE', 'Crinkle': 'CRINKLE', 'Slub':"MC"}
 
     #*-------------------------------------------------------------------------------------------------------------------------*#
-    #! selection criteria
-    col1, col2, col3 = st.columns(3)
-    warp_fibre_select = col1.selectbox("Fibre", list(fibre_dict), help="Dropdown list of fibres used in warp")
-    warp_count_select = str(st.select_slider("Count", count_list, help="Count selector: Warp"))
-    warp_spin_select = st.selectbox("Spinning technology", list(spin_dict),  help="Spinning technology of warp")
-    warp_ply_check = st.checkbox(f'Doube ply: {warp_count_select} Ne', key=1, help=f"Check for doube ply in {warp_count_select} Ne")
-    if warp_ply_check:
-        warp_value = '2/' + warp_count_select
-    else:
-        warp_value = warp_count_select
-    warp_regex = '^'+warp_value+spin_dict.get(warp_spin_select)
-    # st.write(warp_regex)
-    same_for_weft = st.checkbox('Same parameters for Weft')
-    weft_fibre_select = st.selectbox("Fibre", list(fibre_dict), help="Dropdown list of fibres used in weft")
-    weft_count_select = str(st.select_slider("Count", count_list, help="Count selector: Weft"))
-    weft_spin_select = st.selectbox("Spinning technology", list(spin_dict),  help="Spinning technology of weft")
-    weft_ply_check = st.checkbox(f'Doube ply: {weft_count_select} Ne', key=2, help=f"Check for doube ply in {weft_count_select} Ne")
-    if same_for_weft:
-        weft_regex = warp_regex
-    else:
-        if weft_ply_check:
-            weft_value = '2/' + weft_count_select
+    if nav_menu=="Enquiry":
+        #! selection criteria
+        col1, col2, col3 = st.columns(3)
+        warp_fibre_select = col1.selectbox("Fibre", list(fibre_dict), help="Dropdown list of fibres used in warp")
+        warp_count_select = str(col1.select_slider("Count", count_list, help="Count selector: Warp"))
+        warp_spin_select = col1.selectbox("Spinning technology", list(spin_dict),  help="Spinning technology of warp")
+        warp_ply_check = col1.checkbox(f'Doube ply: {warp_count_select} Ne', key=1, help=f"Check for doube ply in {warp_count_select} Ne")
+        if warp_ply_check:
+            warp_value = '2/' + warp_count_select
         else:
-            weft_value = weft_count_select
-        weft_regex = '^'+weft_value+spin_dict.get(weft_spin_select)
-    # st.write(weft_regex)
-    epi_range = st.slider('EPI range', 50, 210, (60, 150))
-    ppi_range = st.slider('PPI range', 50, 200, (60, 150))
-    weave_selectbox = st.selectbox("Weave", weave_list, help="Select the fabric weave")
-    effect_selectbox = st.selectbox("Effect", list(effect_dict), help="Select any special effect on fabric")
-    gsm_range = st.slider('GSM range', 120, 350, (150, 200))
+            warp_value = warp_count_select
+        warp_regex = '^'+warp_value+spin_dict.get(warp_spin_select)
+        # st.write(warp_regex)
+        same_for_weft = col1.checkbox('Same parameters for Weft')
+        weft_fibre_select = col2.selectbox("Fibre", list(fibre_dict), help="Dropdown list of fibres used in weft")
+        weft_count_select = str(col2.select_slider("Count", count_list, help="Count selector: Weft"))
+        weft_spin_select = col2.selectbox("Spinning technology", list(spin_dict),  help="Spinning technology of weft")
+        weft_ply_check = col2.checkbox(f'Doube ply: {weft_count_select} Ne', key=2, help=f"Check for doube ply in {weft_count_select} Ne")
+        if same_for_weft:
+            weft_regex = warp_regex
+        else:
+            if weft_ply_check:
+                weft_value = '2/' + weft_count_select
+            else:
+                weft_value = weft_count_select
+            weft_regex = '^'+weft_value+spin_dict.get(weft_spin_select)
+        # st.write(weft_regex)
+        epi_range = col3.slider('EPI range', 50, 210, (60, 150))
+        ppi_range = col3.slider('PPI range', 50, 200, (60, 150))
+        weave_selectbox = col3.selectbox("Weave", weave_list, help="Select the fabric weave")
+        effect_selectbox = col3.selectbox("Effect", list(effect_dict), help="Select any special effect on fabric")
+        gsm_range = col3.slider('GSM range', 120, 350, (150, 200))
 
-    selection_df = article_df[article_df['Construction'].str.contains(weave_selectbox) &
-                                article_df['Construction'].str.contains(effect_dict.get(effect_selectbox))]
+        selection_df = article_df[article_df['Construction'].str.contains(weave_selectbox) &
+                                    article_df['Construction'].str.contains(effect_dict.get(effect_selectbox))]
 
-    # selection_df = selection_df[selection_df['gsm'].between(gsm_range[0], gsm_range[1]) & selection_df['epi'].between(epi_range[0], epi_range[1]) & selection_df['ppi'].between(ppi_range[0], ppi_range[1])]
-    # selection_df = article_df[article_df['warp'].str.contains('^6[a-zA-Z]*', na=False)]
-    #*-------------------------------------------------------------------------------------------------------------------------*#
+        # selection_df = selection_df[selection_df['gsm'].between(gsm_range[0], gsm_range[1]) & selection_df['epi'].between(epi_range[0], epi_range[1]) & selection_df['ppi'].between(ppi_range[0], ppi_range[1])]
+        # selection_df = article_df[article_df['warp'].str.contains('^6[a-zA-Z]*', na=False)]
+        #*-------------------------------------------------------------------------------------------------------------------------*#
 
-    #! dataframe display
-    tab1, tab2 = st.tabs(['Filtered Data', 'All Data'])
-    with tab1:
-        pass
-        selection_df = selection_df.set_index('K1')
-        df_display = st.dataframe(selection_df)
-    with tab2:
-        df_display = st.dataframe(articles_df)
+        #! dataframe display
+        tab1, tab2 = st.tabs(['Filtered Data', 'All Data'])
+        with tab1:
+            pass
+            selection_df = selection_df.set_index('K1')
+            df_display = st.dataframe(selection_df)
+        with tab2:
+            df_display = st.dataframe(articles_df)
 
     st.divider()
 
