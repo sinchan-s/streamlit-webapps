d4c5c36f113ad2da462711d881ec2b6360b9aef5
Sinchan <sinchan.tex@gmail.com>
2024-02-04 00:08:45 +0530

    graph testing #4

--- st_dashboards/qa/pages/QA_Reports.py
+++ st_dashboards/qa/pages/QA_Reports.py
@@ -99,44 +99,50 @@ if selected=='Grouping':
     qa_file_list = [item for item in drive_files_list if re.findall('qa-',item)]
     qa_file_select = st.multiselect('Select files:', qa_file_list, default=qa_file_list[-1], key=12)
     workbooks = []
+
+    #!----testing section
+    # for i,d in enumerate(qa_file_select):
+        # workbooks.append(pd.read_excel('st_dashboards/qa/files/'+d, sheet_name='Summary'))
+    # with st.expander('File Preview', expanded=False):
+    #     st.write(workbooks)
+
+    #!-----columnized file data display
+    col1, col2 = st.columns(2, gap='large')
+    delta_val1, delta_val2, delta_val3 = 0, 0, 0
+    print_production, yd_production, months, print_q3_list, yd_q3_list = [], [], [], [], []
     for i,d in enumerate(qa_file_select):
         st.toast(f"Analyzing '{d}' ..........")
-        # workbooks.append(pd.read_excel('st_dashboards/qa/files/'+d, sheet_name='Summary'))
         workbooks.append(pd.read_excel(drive_fetch(d).read(), sheet_name='Summary'))
-        st.toast(f"Loaded '{d}' !!")
-
-    with st.expander('File Preview', expanded=False):
-        st.write(workbooks)
-    #!-----columnized file data display
-    col1, col2, col3 = st.columns(3, gap='large')
-    delta_val1, delta_val2, delta_val3 = 0, 0, 0
-    print_production_vals, yd_production_vals = [], []    
-    for i,d in enumerate(qa_file_select):
         summary_sheet = workbooks[i]
         df_month = d.split('.')[0].split('-')[1].upper()
+        months.append(df_month)
         print_prod = summary_sheet.iloc[5,3]
-        print_production_vals.append(print_prod)
+        print_production.append(print_prod)
         print_q3 = summary_sheet.iloc[17:20,6].sum()+summary_sheet.iloc[21,6]
+        print_q3_list.append(print_q3)
         yd_prod = summary_sheet.iloc[5,7]
-        yd_production_vals.append(yd_prod)
+        yd_production.append(yd_prod)
         yd_q3 = summary_sheet.iloc[25,6]+summary_sheet.iloc[26,6]
-        total_prod = print_prod + yd_prod
-        delta_val1 = (total_prod - delta_val1)*100/delta_val1 if delta_val1 != 0 else 0
-        delta_val2 = (print_prod - delta_val2)*100/delta_val2 if delta_val2 != 0 else 0
-        delta_val3 = (yd_prod - delta_val3)*100/delta_val3 if delta_val3 != 0 else 0
-        col1.metric(f":blue[Total Production] : :grey[{df_month}]", f"{total_prod:,.2f} m", delta=f'{delta_val1:#.1f} %')
-        delta_val1 = total_prod
-        col2.metric(f":orange[Print Production] : :grey[{df_month}]", f"{print_prod:,.2f} m", delta=f'{delta_val2:.1f} %')
-        delta_val2 = print_prod
-        col3.metric(f":violet[YD Production] : :grey[{df_month}]", f"{yd_prod:,.2f} m", delta=f'{delta_val3:.1f} %')
-        delta_val3 = yd_prod
-        col2.metric(f":red[Print Q3] : :grey[{df_month}]", f"{print_q3*100:,.2f} %")
-        col3.metric(f":red[YD Q3] : :grey[{df_month}]", f"{yd_q3*100:,.2f} %")
-    compiled_production_data =pd.DataFrame([print_production_vals, yd_production_vals],  columns=["Print", "Yarn-Dyed"])
-    compiled_production_data
-    compiled_q3_data =pd.DataFrame([[print_q3*100, yd_q3*100]],  columns=["Print", "Yarn-Dyed"])
+        yd_q3_list.append(yd_q3)
+        # total_prod = print_prod + yd_prod
+        # delta_val1 = (total_prod - delta_val1)*100/delta_val1 if delta_val1 != 0 else 0
+        # delta_val2 = (print_prod - delta_val2)*100/delta_val2 if delta_val2 != 0 else 0
+        # delta_val3 = (yd_prod - delta_val3)*100/delta_val3 if delta_val3 != 0 else 0
+        # col1.metric(f":blue[Total Production] : :grey[{df_month}]", f"{total_prod:,.2f} m", delta=f'{delta_val1:#.1f} %')
+        # delta_val1 = total_prod
+        # col2.metric(f":orange[Print Production] : :grey[{df_month}]", f"{print_prod:,.2f} m", delta=f'{delta_val2:.1f} %')
+        # delta_val2 = print_prod
+        # col3.metric(f":violet[YD Production] : :grey[{df_month}]", f"{yd_prod:,.2f} m", delta=f'{delta_val3:.1f} %')
+        # delta_val3 = yd_prod
+        # col2.metric(f":red[Print Q3] : :grey[{df_month}]", f"{print_q3*100:,.2f} %")
+        # col3.metric(f":red[YD Q3] : :grey[{df_month}]", f"{yd_q3*100:,.2f} %")
+        st.toast(f"Loaded '{d}' !!")
+    compiled_production_data =pd.DataFrame({'Print':print_production, 'Yarn-Dyed':yd_production}, index=months)
+    compiled_q3_data =pd.DataFrame({"Print":print_q3_list, "Yarn Dyed":yd_q3_list}, index=months)
+    col1.subheader("Monthly Production")
     col1.bar_chart(data=compiled_production_data, color=["#FF2255", "#5522FF"])
-    col2.bar_chart(data=compiled_q3_data, color=["#FF2255", "#5522FF"])
+    col2.subheader("Monthly Q3")
+    col2.bar_chart(data=compiled_q3_data.mul(100), color=["#FF2255", "#5522FF"])
 
 
 if selected=='Lab':
